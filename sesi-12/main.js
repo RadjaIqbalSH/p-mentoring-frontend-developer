4aa6ae1a5142fd98b162b9f493f00410e90ed12f
Radja Iqbal Sanusi Harahap <radja@femaledaily.com>
2026-04-16 20:05:52 +0700

    feat: sesi 12.1

---
+++ sesi-12/main.js
@@ -0,0 +1,75 @@
+/**
+ * Jeni jenis console
+ */
+
+// console.log("Hallo World");
+// console.warn("Hallo World");
+// console.error("Error 1");
+// console.table({
+//   name: "HsirisH",
+//   age: 1
+// })
+// console.info("Info");
+// console.count("Hallo");
+
+/**
+ * Function
+ */
+
+function sum() {
+  return 10 + 2;
+}
+// console.log("hasil function sum => ", sum())
+
+/**
+ * function + parameter
+*/
+const nilai1 = 10
+const nilai2 = 2
+const nilai3 = 10;
+const nilai4 = 5;
+
+// function tambahan(param5) {
+//   return 5 + param5
+// }
+
+// function childHallo (param3) {
+//   return param3;
+// }
+
+// function halloWorld1(param1, param3, param2) {
+//   return param1 + childHallo(param2) + param3;
+// };
+
+// console.log("Nilai = ", halloWorld1(1,2,tambahan(6)))
+
+// declaration func
+function exampleFunc (param1) {
+  console.log("Kapal => ", param1);
+};
+
+// arrow func
+const exampleFunc2 = () => {
+  console.log("Mobil");
+};
+
+// expression func
+const exampleFunc3 = function () {
+  console.log("Pesawat");
+};
+
+// exampleFunc();
+// exampleFunc2();
+// exampleFunc3();
+
+// const example = halloWorld1;
+
+// function sum(param1, param2, param3) {
+//   console.log("type data 1 => ", typeof param1)
+//   console.log("type data 2 => ", typeof param2)
+//   console.log("type data 3 => ", param3)
+// }
+
+// sum("Jhon", 50, halloWorld1());
+
+// console.log("hasil function sum 2-1  => ", sum())
