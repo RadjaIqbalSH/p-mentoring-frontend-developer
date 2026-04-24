84edf1ca63474d5e4c592bd5246879fe5e576bc3
Radja Iqbal Sanusi Harahap <radja@femaledaily.com>
2026-04-24 19:55:16 +0700

    feat: sesi 12.2

--- sesi-12/main.js
+++ sesi-12/main.js
@@ -58,6 +58,48 @@ const exampleFunc3 = function () {
   console.log("Pesawat");
 };
 
+// Sebuah function yang bertujuan untuk menghasilkan sesuatu
+function withReturn (qty, price) {
+  console.log("Function with return")
+
+  const totalBelanja = qty * price
+
+  if (qty < 1) {
+    return "Tidak Valid";
+  }
+
+  if (totalBelanja > 100000) {
+    return totalBelanjaWithDiscount = totalBelanja - 20000
+  } else {
+    return totalBelanja
+  }
+}
+
+
+// Sebuah function yang bertujuan untuk melakukan suatu tugas
+function withoutReturn () {
+
+  const element = document.getElementById("parap")
+
+  if (!element) return; 
+
+  document.getElementById("parap").textContent = "Sudah di Klik"
+
+}
+
+withoutReturn();
+
+// document.getElementById("btn").addEventListener("click", withoutReturn)
+// document.getElementById("btn2").addEventListener("click", withoutReturn)
+
+
+// const resultFuncWithReturn = withReturn(0 , 30000);
+// const resultFuncWithoutReturn = withoutReturn();
+
+
+// console.log("resultFuncWithReturn => ", resultFuncWithReturn)
+// console.log("resultFuncWithoutReturn => ", resultFuncWithoutReturn)
+
 // exampleFunc();
 // exampleFunc2();
 // exampleFunc3();
@@ -73,3 +115,152 @@ const exampleFunc3 = function () {
 // sum("Jhon", 50, halloWorld1());
 
 // console.log("hasil function sum 2-1  => ", sum())
+
+/**
+ * Jenis jenis function
+ * 
+ */
+
+// declaration function
+function thisIsDeclarationFunction () {
+  console.log("Hallo World")
+}
+
+// Expression function
+const thisIsExpressionFunction = function () {
+
+}
+
+// Arrow function
+const thisIsArrowFunction = () => {
+  console.log("thisIsArrowFunction -> ", this)
+}
+
+/**
+ * 
+ * Callback function
+ */
+
+function callbackFunc () {
+  console.log("Hallo World")
+}
+
+function thisIsCallbackFunction (param1) {
+  param1();
+}
+
+// thisIsCallbackFunction(callbackFunc)
+
+
+/**
+ * 
+ * Type data Array
+ * 
+ */
+
+const dataDiri = {
+  nama : "Jhon",
+  age: 50,
+  gender: ["Laki Laki", "Perempuan"],
+  location: {
+    city: "Jakarta",
+    postalCode: "20141",
+    house: {
+      color: "red",
+      number: 12
+    }
+  },
+  exampleFunc: function () {
+    console.log("example function")
+  }
+}
+
+// dataDiri.exampleFunc();
+
+// console.log("Gender => ", dataDiri.gender[1])
+// console.log("data diri age => ", dataDiri.age)
+// console.log("data diri nama => ", dataDiri.nama)
+// console.log("data diri city => ", dataDiri.location.city)
+// console.log("data diri nomor rumah => ", dataDiri.location.house.number)
+
+const product = {
+  name: "Sunscreen",
+  price: 12000,
+  stock: 100,
+  isDiscount: true,
+};
+
+/**
+ * 
+ * Type data array
+ * 
+ */
+
+const example = [
+  "car", 
+  "house", 
+  "paint", 
+  ["cat", "dog"], 
+  {
+    nama : "Jhon",
+    age: 50,
+    gender: "Laki Laki",
+  },
+  function () {
+    console.log("Hallo World")
+  }
+]
+
+// for (let i = 1; i < 5; i++) {
+//   console.log("Loop => ", i)
+// }
+
+let a = 1
+
+// while (a < 5) {
+//   console.log("while ==> ", a);
+//   a = a + 1;
+// }
+
+// console.log("example -> ", example[0])
+// console.log("example -> ", example[1])
+// console.log("example -> ", example[2])
+// console.log("example -> ", example[3][1])
+// console.log("example -> ", example[4].gender)
+
+// example[5]();
+
+const listNumber = ["car","house","food","cat"];
+const tempListNumber = []
+let index = 0
+
+for (const bucket of listNumber) {
+  // break;
+
+  // if (bucket === "food") {
+  //   break;
+  // }
+
+  console.log("bucket value => ", bucket)
+  tempListNumber[index] = "name -> " + bucket
+  index++
+}
+
+console.log("tempListNumber => ", tempListNumber)
+
+listNumber.forEach(function (value, index) {
+  // console.log("For Each value => ", value)
+  // console.log("For Each index => ", index)
+})
+
+listNumber.map(function(value, index) {
+  console.log("map value => ", value)
+  console.log("map index => ", index)
+})
+
+const mapListNumber = listNumber.map(function(value) {
+  return "name > " + value
+})
+
+console.log("mapListNumber -> ", mapListNumber)
+
